6fe71e7ba539e095d0482f2d9f71de918ed8ccf1
MakarukArsen <makarukarsen@gmail.com>
2022-07-10 21:54:55 +0300

    start2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import PostComponent from './Post';
 
 function App() {
+
   const ANAKIN_IMAGE = "https://media.gq-magazine.co.uk/photos/620529e268071f7ecff06fac/1:1/w_1080,h_1080,c_limit/100222_Bobba_hp.jpg";
   const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
   const post = [
@@ -16,7 +17,7 @@ function App() {
       reposts: "182",
       likes: "887",
     }
-  ];
+  ]
   return (
     <div className="App">
       <PostComponent data={post[0]}></PostComponent>
